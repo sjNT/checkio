f8ea9ac9d0ab7cbaac2399c8fa25f31dbf903ecc
sjNT <sjnt@live.com>
2019-06-18 14:40:53 +0700

    .

---
+++ Elementary/Second Index.py
@@ -0,0 +1,19 @@
+def second_index(text: str, symbol: str) -> [int, None]:
+    if symbol in text and text.count(symbol) >= 2:
+        li = list(text)
+        li.pop(li.index(symbol))
+        return li.index(symbol) + 1
+    return None
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
